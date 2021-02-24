c39881d1f01de7e9a0a85b4b01549768c53f06ab
Motoyuki Koseki <motoyuki.koseki@gmail.com>
2021-02-24 13:36:41 +0000

    [Update]ユーザートップ画面の変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   helper_method :followings_counter, :followers_counter
 
   def followings_counter
-    current_user.followings.count
+    current_user.followings.where.not(status: 3).count
   end
 
   def followers_counter
-    Following.where(following_user_id: current_user.id).count
+    Following.where(following_user_id: current_user.id).where.not(status: 3).count
   end
 
 end
