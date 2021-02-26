1660a1c3ce4e5aa55282b20f86f9b7f073384ed9
Motoyuki Koseki <motoyuki.koseki@gmail.com>
2021-02-26 09:37:58 +0000

    [Fix]タイムラインに表示する項目の修正

--- app/controllers/public/timelines_controller.rb
+++ app/controllers/public/timelines_controller.rb
@@ -7,13 +7,14 @@ class Public::TimelinesController < ApplicationController
 
   def item
     @following_user_ids = current_user.followings.where.not(status: 2).pluck(:following_user_id)
-    @items = Item.where(user_id: @following_user_ids).includes(:user).where(users: {status: 0}).order(id: "DESC")
+    @active_user_ids = User.where(id: @following_user_ids, status: 0).ids
+    @scene_items = SceneItem.includes(:item).where(items: {user_id: @active_user_ids}).order(id: "DESC")
   end
 
   def review
     @following_user_ids = current_user.followings.where.not(status: 2).pluck(:following_user_id)
-    @review_item_ids = Review.pluck(:item_id)
-    @items = Item.where(id: @review_item_ids, user_id: @following_user_ids).includes(:user).where(users: {status: 0}).order(id: "DESC")
+    @active_user_ids = User.where(id: @following_user_ids, status: 0).ids
+    @reviews = Review.includes(:item).where(items: {user_id: @active_user_ids}).order(id: "DESC")
   end
 
 end
