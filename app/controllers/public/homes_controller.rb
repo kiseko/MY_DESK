87433ba98f11f5341a4b6f3b2d5b423a9fd4ed3d
Motoyuki Koseki <motoyuki.koseki@gmail.com>
2021-02-28 15:23:37 +0000

    [Fix]無限スクロール機能の修正

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,7 +2,7 @@ class Public::HomesController < ApplicationController
 
   def top
     @scene_user_ids = Scene.pluck(:user_id)
-    @users = User.where(id: @scene_user_ids).order(id: "DESC")
+    @users = User.where(id: @scene_user_ids).order(id: "DESC").page(params[:page]).per(8)
   end
 
   def about
