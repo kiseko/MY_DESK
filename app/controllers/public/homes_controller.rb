fb3572455f637a42cce8278b12952bbe25b2cc03
Motoyuki Koseki <motoyuki.koseki@gmail.com>
2021-03-04 19:07:53 +0000

    [Update]Homesコントローラーの変更

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,7 +2,7 @@ class Public::HomesController < ApplicationController
 
   def top
     @scene_user_ids = Scene.pluck(:user_id)
-    @users = User.where(id: @scene_user_ids).order(id: "DESC").page(params[:page]).per(8)
+    @users = User.where(id: @scene_user_ids, status: 0).order(id: "DESC").page(params[:page]).per(8)
   end
 
   def about
